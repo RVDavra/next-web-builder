73a87ac6814e91962daf5c8b5ac2f61351e3e13c
RVDavra <riteshdavra@gmail.com>
2022-05-12 18:14:01 +0530

    add undo redo functionality

--- src/App.js
+++ src/App.js
@@ -11,17 +11,7 @@ function App() {
   return (
     <GrapesjsReact
       id="grapesjs-react"
-      onInit={(editor) => {
-        editor.Panels.addButton("options", {
-          id: "update-theme",
-          className: "fa fa-adjust",
-          command: "open-update-theme",
-          attributes: {
-            title: "Update Theme",
-            "data-tooltip-pos": "bottom",
-          },
-        });
-      }}
+      onInit={loadPanels}
       blockManager={blockManager}
       plugins={[tailWIndPlugin]}
       height={"100vh"}
@@ -34,5 +24,73 @@ function App() {
     />
   );
 }
+
+export const loadPanels = (editor) => {
+  // Show Style Manager
+  editor.on("component:selected", () => {
+    const openSmBtn = editor.Panels.getButton("views", "open-sm");
+    const openLayersBtn = editor.Panels.getButton("views", "open-layers");
+
+    // Don't switch when the Layer Manager is on or there is no selected component
+    if (
+      (!openLayersBtn || !openLayersBtn.get("active")) &&
+      editor.getSelected()
+    ) {
+      openSmBtn && openSmBtn.set("active", 1);
+    }
+  });
+
+  editor.on("load", () => editor.Panels.getButton("views", "open-blocks").set("active", 1));
+
+  editor.Commands.add("canvas-clear", (e) => {
+    // eslint-disable-next-line no-restricted-globals
+    const result = confirm("Do you want to clear the canvas?");
+    if (result) {
+      e.runCommand("core:canvas-clear");
+    }
+  });
+
+  // Config Buttons
+  editor.Panels.removeButton("options", "export-template");
+
+  editor.Panels.getButton("options", "sw-visibility").set("active", false);
+
+  editor.Panels.addButton("options", {
+    id: "export-template",
+    className: "fa fa-code",
+    command: (e) => e.runCommand("export-template"),
+    attributes: { title: "View Code" },
+  });
+
+  editor.Panels.addButton("options", {
+    id: "undo",
+    className: "fa fa-undo",
+    command: (e) => e.runCommand("core:undo"),
+    attributes: { title: "Undo" },
+  });
+
+  editor.Panels.addButton("options", {
+    id: "redo",
+    className: "fa fa-repeat",
+    command: "core:redo",
+    attributes: { title: "Redo" },
+  });
+
+  editor.Panels.addButton("options", {
+    id: "canvas-clear",
+    className: "fa fa-trash",
+    command: (e) => e.runCommand("canvas-clear"),
+  });
+
+  editor.Panels.addButton("options", {
+    id: "update-theme",
+    className: "fa fa-adjust",
+    command: "open-update-theme",
+    attributes: {
+      title: "Update Theme",
+      "data-tooltip-pos": "bottom",
+    },
+  });
+};
 
 export default App;
